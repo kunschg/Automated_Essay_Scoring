1ce84c39701fe8b62a947c60817737475c31eab7
gisship <hippodingos@gmail.com>
2023-04-08 17:48:39 +0200

    Added transformer model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -215,4 +216,59 @@ class ConvNet1Dv3(nn.Module):
         x = torch.sigmoid(self.fc_final(x))
         x = x.view(x.shape[0])
         
+        return x
+    
+###################
+# Transformer model
+###################
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, input_channels, max_essay_length, dropout):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        position = torch.arange(max_essay_length).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, input_channels, 2) * (-math.log(10000.0) / input_channels))
+        pe = torch.zeros(max_essay_length, input_channels)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.transpose(1,0)        
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + self.pe
+        return self.dropout(x)
+
+class TransformerModel(nn.Module):
+    def __init__(self, input_channels, max_essay_length, num_heads, hidden_dim, num_layers, dropout):
+        super().__init__()
+
+        self.embedding_dim = input_channels
+        self.max_essay_length = max_essay_length
+        self.num_heads = num_heads
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.pos_encoder = PositionalEncoding(input_channels, max_essay_length, dropout)
+        self.transformer_encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(input_channels, num_heads, hidden_dim, dropout), num_layers)
+        self.fc_essay = nn.Linear(input_channels * max_essay_length, 1)
+        self.fc_essay_set = nn.Linear(8,1)
+        self.fc_final = nn.Linear(2,1)
+
+    def forward(self, essays, essay_sets):
+        # Essay branch
+        x1 = self.pos_encoder(essays).transpose(1,2)
+        x1 = self.transformer_encoder(x1)
+        x1 = x1.view(-1, self.embedding_dim * self.max_essay_length)
+        x1 = self.fc_essay(x1)  
+
+        # Essay_set branch
+        x2 = F.one_hot(essay_sets-1, num_classes=8).view(essay_sets.shape[0],8).float()
+        x2 = self.fc_essay_set(x2)
+
+        # Concatenation and prediction
+        x = torch.cat((x1, x2), axis = 1)
+        x = torch.sigmoid(self.fc_final(x).view(-1))
+
         return x
