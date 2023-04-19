d8a45a506f79cacdda13e2a23bf7eb4ec886bfc0
guillaumekunsch <guillaume.kunsch@qantev.com>
2023-04-19 16:08:27 +0200

    try 1st run on the whole pipeline + add linting to .py files + add config for mps backend in train.py

--- models.py
+++ models.py
@@ -1,98 +1,131 @@
-import numpy as np
 import math
+import os
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import TensorDataset, ConcatDataset, DataLoader
+from torch.utils.data import ConcatDataset, DataLoader, TensorDataset
 from tqdm import tqdm
-import os
 
 #####################
 # Convolutional model
 #####################
 
+
 class ConvNet1D(nn.Module):
-    def __init__(self, input_channels, depth, kernel_size, max_essay_length, skip_connections, batch_norm):
+    def __init__(
+        self,
+        input_channels,
+        depth,
+        kernel_size,
+        max_essay_length,
+        skip_connections,
+        batch_norm,
+    ):
         super().__init__()
         self.input_channels = input_channels
         self.depth = depth
-        self.conv_channels = [self.input_channels]*(self.depth+1) + [1]
+        self.conv_channels = [self.input_channels] * (self.depth + 1) + [1]
         self.kernel_size = kernel_size
         self.max_essay_length = max_essay_length
         self.skip_connections = skip_connections
         self.batch_norm = batch_norm
-        
+
         # Convolutions
-        self.convs = nn.ModuleList([nn.Conv1d(
-            in_channels = self.conv_channels[i], 
-            out_channels = self.conv_channels[i+1],
-            kernel_size = self.kernel_size,
-            padding = 'same') for i in range(len(self.conv_channels)-1)])
+        self.convs = nn.ModuleList(
+            [
+                nn.Conv1d(
+                    in_channels=self.conv_channels[i],
+                    out_channels=self.conv_channels[i + 1],
+                    kernel_size=self.kernel_size,
+                    padding="same",
+                )
+                for i in range(len(self.conv_channels) - 1)
+            ]
+        )
         self.bnorm1d = nn.BatchNorm1d(self.input_channels)
 
         ## Full connections
         self.fc_emb = nn.Linear(8, self.max_essay_length)
-        self.fc_final = nn.Linear(self.max_essay_length,1)
-        
+        self.fc_final = nn.Linear(self.max_essay_length, 1)
+
     def forward(self, essays, essay_sets):
         x1 = essays
-        x2 = F.one_hot(essay_sets-1, num_classes=8).view(essay_sets.shape[0],1,8).float()
+        x2 = (
+            F.one_hot(essay_sets - 1, num_classes=8)
+            .view(essay_sets.shape[0], 1, 8)
+            .float()
+        )
         x2 = F.relu(self.fc_emb(x2))
         x = x1 + x2
 
         if self.skip_connections:
             if self.batch_norm:
-                for i, conv in enumerate(self.convs): 
-                    if self.conv_channels[i] == self.conv_channels[i+1]:
+                for i, conv in enumerate(self.convs):
+                    if self.conv_channels[i] == self.conv_channels[i + 1]:
                         x = F.relu(self.bnorm1d(conv(x))) + x
                     else:
-                        x = F.relu(conv(x))                
+                        x = F.relu(conv(x))
             else:
-                for i, conv in enumerate(self.convs): 
-                    if self.conv_channels[i] == self.conv_channels[i+1]:
+                for i, conv in enumerate(self.convs):
+                    if self.conv_channels[i] == self.conv_channels[i + 1]:
                         x = F.relu(conv(x)) + x
                     else:
                         x = F.relu(conv(x))
-        else:   
+        else:
             if self.batch_norm:
                 for i, conv in enumerate(self.convs):
-                    if self.conv_channels[i] == self.conv_channels[i+1]:
+                    if self.conv_channels[i] == self.conv_channels[i + 1]:
                         x = F.relu(self.bnorm1d(conv(x)))
                     else:
                         x = F.relu(conv(x))
-            else:            
+            else:
                 for conv in self.convs:
                     x = F.relu(conv(x))
 
-        x = x.view(x.shape[0],-1)
+        x = x.view(x.shape[0], -1)
         x = torch.sigmoid(self.fc_final(x))
         x = x.view(x.shape[0])
-        
+
         return x
-    
+
+
 ###################
 # Transformer model
 ###################
+
 
 class PositionalEncoding(nn.Module):
     def __init__(self, input_channels, max_essay_length, dropout):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
-        
+
         position = torch.arange(max_essay_length).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, input_channels, 2) * (-math.log(10000.0) / input_channels))
+        div_term = torch.exp(
+            torch.arange(0, input_channels, 2) * (-math.log(10000.0) / input_channels)
+        )
         pe = torch.zeros(max_essay_length, input_channels)
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.transpose(1,0)        
-        self.register_buffer('pe', pe)
-        
+        pe = pe.transpose(1, 0)
+        self.register_buffer("pe", pe)
+
     def forward(self, x):
         x = x + self.pe
         return self.dropout(x)
 
+
 class TransformerModel(nn.Module):
-    def __init__(self, input_channels, max_essay_length, num_heads, hidden_dim, num_layers, dropout):
+    def __init__(
+        self,
+        input_channels,
+        max_essay_length,
+        num_heads,
+        hidden_dim,
+        num_layers,
+        dropout,
+    ):
         super().__init__()
 
         self.embedding_dim = input_channels
@@ -103,34 +136,44 @@ class TransformerModel(nn.Module):
         self.dropout = dropout
         self.pos_encoder = PositionalEncoding(input_channels, max_essay_length, dropout)
         self.transformer_encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(input_channels, num_heads, hidden_dim, dropout), num_layers)
+            nn.TransformerEncoderLayer(input_channels, num_heads, hidden_dim, dropout),
+            num_layers,
+        )
         self.fc_essay_set = nn.Linear(8, max_essay_length)
         self.fc_final = nn.Linear(input_channels * max_essay_length, 1)
 
     def forward(self, essays, essay_sets):
         # Add positional encoding to essays
         x1 = self.pos_encoder(essays)
-        
+
         # Add essay_set embedding
-        x2 = F.one_hot(essay_sets-1, num_classes=8).view(essay_sets.shape[0],8).float()
+        x2 = (
+            F.one_hot(essay_sets - 1, num_classes=8)
+            .view(essay_sets.shape[0], 8)
+            .float()
+        )
         x2 = F.relu(self.fc_essay_set(x2))
         x2 = x2.view(-1, 1, x2.shape[1])
 
         # Concatenate and predict
         x = x1 + x2
-        x = x.transpose(2,1)
+        x = x.transpose(2, 1)
         x = self.transformer_encoder(x)
         x = x.view(-1, self.embedding_dim * self.max_essay_length)
         x = torch.sigmoid(self.fc_final(x)).view(-1)
 
         return x
 
+
 #########################
 # Transfer learning model
 #########################
 
+
 class SwinTransformerModelTL(nn.Module):
-    def __init__(self, input_channels, max_essay_length, kernel_size, stride, pretrained_model):
+    def __init__(
+        self, input_channels, max_essay_length, kernel_size, stride, pretrained_model
+    ):
         super().__init__()
 
         self.input_channels = input_channels
@@ -138,15 +181,23 @@ class SwinTransformerModelTL(nn.Module):
         self.kernel_size = kernel_size
         self.stride = stride
         self.pretrained_model = pretrained_model
-        self.pretrained_model.features[0][0] = nn.Conv2d(input_channels, 96, kernel_size=(1,kernel_size), stride=stride)
-        self.pretrained_model.head = nn.Linear(in_features=768, out_features=1, bias=True)
+        self.pretrained_model.features[0][0] = nn.Conv2d(
+            input_channels, 96, kernel_size=(1, kernel_size), stride=stride
+        )
+        self.pretrained_model.head = nn.Linear(
+            in_features=768, out_features=1, bias=True
+        )
         self.fc_essay_set = nn.Linear(8, max_essay_length)
 
     def forward(self, essays, essay_sets):
         x1 = essays.view(-1, essays.shape[1], 1, essays.shape[2])
-        x2 = F.one_hot(essay_sets-1, num_classes=8).view(essay_sets.shape[0],8).float()
+        x2 = (
+            F.one_hot(essay_sets - 1, num_classes=8)
+            .view(essay_sets.shape[0], 8)
+            .float()
+        )
         x2 = F.relu(self.fc_essay_set(x2))
-        x2 = x2.view(-1,1,1,x2.shape[1])
+        x2 = x2.view(-1, 1, 1, x2.shape[1])
         x = x1 + x2
         x = torch.sigmoid(self.pretrained_model(x)).view(-1)
 
--- train.py
+++ train.py
@@ -1,117 +1,147 @@
+import argparse
+import os
+import warnings
+
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import TensorDataset, ConcatDataset, DataLoader
-from tqdm import tqdm
-import os
-import argparse
-from models import ConvNet1D, TransformerModel, SwinTransformerModelTL
 import torchvision.models as models
-import warnings
-warnings.filterwarnings('ignore')
+from torch.utils.data import ConcatDataset, DataLoader, TensorDataset
+from tqdm import tqdm
+
+from models import ConvNet1D, SwinTransformerModelTL, TransformerModel
+
+warnings.filterwarnings("ignore")
 
 ############
 # Parameters
 ############
 
+
 def arg_parser():
     parser = argparse.ArgumentParser()
-    
+
     # General training arguments
-    parser.add_argument('--local-machine', action='store_true') 
-    parser.add_argument("--embedder", type=str, default='w2v')
+    parser.add_argument("--local-machine", default=True, action="store_true")
+    parser.add_argument("--embedder", type=str, default="w2v")
     parser.add_argument("--batch-size", type=int, default=64)
-    parser.add_argument("--optimizer", type=str, default='Adam')
+    parser.add_argument("--optimizer", type=str, default="Adam")
     parser.add_argument("--l-rate", type=float, default=1e-3)
-    parser.add_argument("--momentum", type=float, default=0.)
+    parser.add_argument("--momentum", type=float, default=0.0)
     parser.add_argument("--n-epochs", type=int, default=100)
     parser.add_argument("--patience", type=int, default=10)
     parser.add_argument("--min-delta", type=float, default=1e-4)
-    parser.add_argument("--model", type=str, default='ConvNet1D')
+    parser.add_argument("--model", type=str, default="ConvNet1D")
     parser.add_argument("--max_essay_length", type=int, default=1266)
     parser.add_argument("--input-channels", type=int, default=50)
-    parser.add_argument('--do-not-save', action='store_true')
-    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument("--do-not-save", action="store_true")
+    parser.add_argument("--seed", type=int, default=1)
 
     # Arguments specific to convolutional models
     parser.add_argument("--depth", type=int, default=0)
     parser.add_argument("--kernel-size", type=int, default=5)
-    parser.add_argument('--skip-connections', action='store_true')
-    parser.add_argument('--batch-norm', action='store_true')
+    parser.add_argument("--skip-connections", action="store_true")
+    parser.add_argument("--batch-norm", action="store_true")
 
     # Arguments specific to transformer models
-    parser.add_argument('--num-heads', type=int, default=5)
-    parser.add_argument('--hidden-dim', type=int, default=2048)
-    parser.add_argument('--num-layers', type=int, default=1)
-    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument("--num-heads", type=int, default=5)
+    parser.add_argument("--hidden-dim", type=int, default=2048)
+    parser.add_argument("--num-layers", type=int, default=1)
+    parser.add_argument("--dropout", type=float, default=0.1)
 
     # Arguments specific to transfer learning models
-    parser.add_argument('--kernel_size_tl', type=int, default=16)
-    parser.add_argument('--stride_tl', type=int, default=16)
+    parser.add_argument("--kernel_size_tl", type=int, default=16)
+    parser.add_argument("--stride_tl", type=int, default=16)
 
     args = parser.parse_args()
     return args
 
+
 ###################
 # Training function
 ###################
+
 
 def train(args):
     # Set seed
     torch.manual_seed(args.seed)
 
     # Import training and validation data sets
-    print('Loading dataset...\n')
-    if args.local_machine:
-      train_dataset = torch.load(f'data/train_{args.embedder}.pt')
-      val_dataset = torch.load(f'data/val_{args.embedder}.pt')
-    else:    
-      train_dataset = torch.load(f'/content/train_{args.embedder}.pt')
-      val_dataset = torch.load(f'/content/val_{args.embedder}.pt')
+    print("Loading dataset...\n")
 
-    train_loader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle = True)
-    val_loader = DataLoader(val_dataset, batch_size = args.batch_size, shuffle = True)
+    if args.local_machine:
+        train_dataset = torch.load(f"data/train_{args.embedder}.pt")
+        val_dataset = torch.load(f"data/val_{args.embedder}.pt")
+    else:
+        train_dataset = torch.load(f"/content/train_{args.embedder}.pt")
+        val_dataset = torch.load(f"/content/val_{args.embedder}.pt")
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
     # Device set-up
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(f'The current processor is: {device}\n')
+    if torch.cuda.is_available():
+        device_var = "cuda:0"
+    elif torch.backends.mps.is_available():
+        device_var = torch.device("mps")
+    else:
+        device_var = "cpu"
+    device = torch.device(device_var)
+    print(f"The current processor is: {device}\n")
 
     # Model initialization
-    if args.model.startswith('ConvNet1D'):
+    if args.model.startswith("ConvNet1D"):
         model = eval(args.model)(
-            args.input_channels, args.depth, args.kernel_size, args.max_essay_length, args.skip_connections,
-            args.batch_norm)
-        model_name = f'{args.model}_depth{args.depth}_seed{args.seed}'
-    
-    elif args.model == 'TransformerModel':
+            args.input_channels,
+            args.depth,
+            args.kernel_size,
+            args.max_essay_length,
+            args.skip_connections,
+            args.batch_norm,
+        )
+        model_name = f"{args.model}_depth{args.depth}_seed{args.seed}"
+
+    elif args.model == "TransformerModel":
         model = eval(args.model)(
-            args.input_channels, args.max_essay_length, args.num_heads, args.hidden_dim, args.num_layers,
-            args.dropout)
-        model_name = f'{args.model}_numlayers{args.num_layers}_seed{args.seed}'
-    
-    elif args.model.startswith('SwinTransformerModelTL'):
+            args.input_channels,
+            args.max_essay_length,
+            args.num_heads,
+            args.hidden_dim,
+            args.num_layers,
+            args.dropout,
+        )
+        model_name = f"{args.model}_numlayers{args.num_layers}_seed{args.seed}"
+
+    elif args.model.startswith("SwinTransformerModelTL"):
         pretrained_model = models.swin_t(pretrained=True)
-        
+
         for param in pretrained_model.parameters():
             param.requires_grad = False
-        
+
         model = eval(args.model)(
-            args.input_channels, args.max_essay_length, args.kernel_size_tl, args.stride_tl, pretrained_model)
-        model_name = f'{args.model}_seed{args.seed}'
-    
+            args.input_channels,
+            args.max_essay_length,
+            args.kernel_size_tl,
+            args.stride_tl,
+            pretrained_model,
+        )
+        model_name = f"{args.model}_seed{args.seed}"
+
     else:
-        raise Exception('Invalid model')
-    
+        raise Exception("Invalid model")
+
     model = model.to(device)
 
     # Optimizer set-up
-    if args.optimizer == 'Adam':
+    if args.optimizer == "Adam":
         optimizer = torch.optim.Adam(model.parameters(), lr=args.l_rate)
-    elif args.optimizer == 'SGD':
-        optimizer = torch.optim.SGD(model.parameters(), lr=args.l_rate, momentum=args.momentum)
+    elif args.optimizer == "SGD":
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.l_rate, momentum=args.momentum
+        )
     else:
-        raise Exception('Invalid optimizer')
+        raise Exception("Invalid optimizer")
 
     # Initialize training metrics
     training_losses, val_losses, val_maes = [], [], []
@@ -119,89 +149,110 @@ def train(args):
     criterion = nn.MSELoss()
 
     with torch.no_grad():
-        print(f'Epoch [{0}/{args.n_epochs}]')
-        
-        print('Training...')
+        print(f"Epoch [{0}/{args.n_epochs}]")
+
+        print("Training...")
         for essays, essay_sets, grades in tqdm(train_loader):
-            essays, essay_sets, grades = essays.to(device), essay_sets.to(device), grades.to(device)
+            essays, essay_sets, grades = (
+                essays.to(device),
+                essay_sets.to(device),
+                grades.to(device),
+            )
             grades_pred = model(essays, essay_sets)
             loss = criterion(grades_pred, grades)
-            training_loss += loss.item()    
-        training_loss = training_loss/len(train_loader)
+            training_loss += loss.item()
+        training_loss = training_loss / len(train_loader)
         training_losses.append(training_loss)
 
-        print('Validation...')
+        print("Validation...")
         for essays, essay_sets, grades in tqdm(val_loader):
-            essays, essay_sets, grades = essays.to(device), essay_sets.to(device), grades.to(device)
+            essays, essay_sets, grades = (
+                essays.to(device),
+                essay_sets.to(device),
+                grades.to(device),
+            )
             grades_pred = model(essays, essay_sets)
             loss = criterion(grades_pred, grades)
             val_loss += loss.item()
-            val_mae += torch.mean(torch.abs(grades_pred-grades)).item()
+            val_mae += torch.mean(torch.abs(grades_pred - grades)).item()
 
-        val_loss = val_loss/len(val_loader)
+        val_loss = val_loss / len(val_loader)
         val_losses.append(val_loss)
-        val_mae = val_mae/len(val_loader)
+        val_mae = val_mae / len(val_loader)
         val_maes.append(val_mae)
-    
-    print(f'Training loss: {training_loss:.4f}, Validation loss: {val_loss:.4f}, Validation MAE: {val_mae:.4f}\n')
+
+    print(
+        f"Training loss: {training_loss:.4f}, Validation loss: {val_loss:.4f}, Validation MAE: {val_mae:.4f}\n"
+    )
 
     # Initialize early stopping metrics
     best_val_mae = val_mae
     wait = 0
 
     # Training process
-    for epoch in range(1, args.n_epochs+1):
-        print(f'Epoch [{epoch}/{args.n_epochs}]')
+    for epoch in range(1, args.n_epochs + 1):
+        print(f"Epoch [{epoch}/{args.n_epochs}]")
         training_loss, val_loss, val_mae = 0, 0, 0
 
         # Training loop
-        print('Training...')
+        print("Training...")
         for essays, essay_sets, grades in tqdm(train_loader):
-            essays, essay_sets, grades = essays.to(device), essay_sets.to(device), grades.to(device)
+            essays, essay_sets, grades = (
+                essays.to(device),
+                essay_sets.to(device),
+                grades.to(device),
+            )
             grades_pred = model(essays, essay_sets)
             loss = criterion(grades_pred, grades)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             training_loss += loss.item()
-        
-        training_loss = training_loss/len(train_loader)
+
+        training_loss = training_loss / len(train_loader)
         training_losses.append(training_loss)
 
         # Validation loop
-        print('Validation...')
+        print("Validation...")
         with torch.no_grad():
             for essays, essay_sets, grades in tqdm(val_loader):
-                essays, essay_sets, grades = essays.to(device), essay_sets.to(device), grades.to(device)
+                essays, essay_sets, grades = (
+                    essays.to(device),
+                    essay_sets.to(device),
+                    grades.to(device),
+                )
                 grades_pred = model(essays, essay_sets)
                 loss = criterion(grades_pred, grades)
                 val_loss += loss.item()
-                val_mae += torch.mean(torch.abs(grades_pred-grades)).item()
-        
-        val_loss = val_loss/len(val_loader)
+                val_mae += torch.mean(torch.abs(grades_pred - grades)).item()
+
+        val_loss = val_loss / len(val_loader)
         val_losses.append(val_loss)
-        val_mae = val_mae/len(val_loader)
+        val_mae = val_mae / len(val_loader)
         val_maes.append(val_mae)
 
-        print(f'Training loss: {training_loss:.4f}, Validation loss: {val_loss:.4f}, Validation MAE: {val_mae:.4f}\n')
+        print(
+            f"Training loss: {training_loss:.4f}, Validation loss: {val_loss:.4f}, Validation MAE: {val_mae:.4f}\n"
+        )
 
         # Check for early stopping
         if val_mae < best_val_mae - args.min_delta:
             best_val_mae = val_mae
             wait = 0
             if not args.do_not_save:
-              torch.save(model.state_dict(), f'models/{model_name}.pth')
+                torch.save(model.state_dict(), f"models/{model_name}.pth")
         else:
             wait += 1
             if wait >= args.patience:
-                print(f'Early stopping after {epoch} epochs')
+                print(f"Early stopping after {epoch} epochs")
                 break
-    
-    print(f'Best validation MAE: {best_val_mae:.4f}')
+
+    print(f"Best validation MAE: {best_val_mae:.4f}")
 
     if not args.do_not_save:
-      metrics = np.concatenate([[training_losses], [val_losses], [val_maes]], axis = 0)
-      np.save('outputs/' + model_name, metrics)
+        metrics = np.concatenate([[training_losses], [val_losses], [val_maes]], axis=0)
+        np.save("outputs/" + model_name, metrics)
+
 
 #############
 # File runner
